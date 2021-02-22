ad0a86cbf7bd9a65d9df0fe9e9fd04e2cf494e44
PaulOnyirimba <Onyirimbap@gmail.com>
2021-02-21 22:56:57 -0500

    testing

---
+++ week1/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>First todo</p>
+            
+            <input type="checkbox" />
+            <p>Second todo</p>
+            
+            <input type="checkbox" />
+            <p>Third todo</p>
+            
+        </div>
+    )
+}
+
+export default App
