99ce0a829f5fd859a55f2827f98a98776c93990d
PaulOnyirimba <Onyirimbap@gmail.com>
2021-04-02 15:34:09 -0400

    testing

---
+++ week2/vacationspots/src/SpotsApp.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import SpotCard from './SpotsCard.js'
+import SpotsData from './SpotsData.js'
+import './SpotsApp.css'
+
+function SpotsApp() {
+    const spotComponents = SpotsData.map(spot =>
+    <SpotCard
+        key={spot.id}
+        place={spot.place}
+        price={spot.price}
+        timeToGo={spot.timeToGo}
+        bgcolor={spot.bgcolor}
+        color={spot.color}
+        />
+    )
+
+    return (
+        <div className = 'SpotsDiv'>
+        {spotComponents}
+        </div>
+    )
+}
+
+export default SpotsApp 
---
+++ week2/vacationspots/src/SpotsData.js
@@ -0,0 +1,41 @@
+const SpotsData = [
+    {
+     id: 1
+     place: "Meridian, Idaho",
+    price: 40,
+    timeToGo: "Spring",
+    bgcolor: "red",
+    color: "white"
+    },
+    {
+     id: 2
+     place: "Cancun",
+    price: 900,
+    timeToGo: "Winter",
+    bgcolor: "orange",
+    color: "white"
+    },
+    {
+     place: "China",
+    price: 1200,
+    timeToGo: "Fall",
+    bgcolor: "black",
+    color: "yellow"
+    },
+    {
+     place: "Russia",
+    price: 1100,
+    timeToGo: "Summer",
+    bgcolor: "blue",
+    color: "white"
+    },
+    {
+     place: "Lebanon",
+    price: 400,
+    timeToGo: "Spring",
+    bgcolor: "green",
+    color: "white"
+    }
+]
+
+export default SpotsData
