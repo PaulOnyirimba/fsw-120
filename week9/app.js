31b36e523743efbeb8ae60c880e881e587f4d499
PaulOnyirimba <Onyirimbap@gmail.com>
2021-04-18 22:41:35 -0400

    testing

---
+++ week9/app.js
@@ -0,0 +1,14 @@
+
+
+const sumFx = require('./math')
+
+console.log(sumFx(9, 3))
+
+const sum2Fx = require('./math')
+console.log(sumFx(10, 2))
+
+const sum3Fx = require('./math')
+console.log(sumFx(4, 2))
+
+const sum4Fx = require('./math')
+console.log(sumFx(9, 3))
